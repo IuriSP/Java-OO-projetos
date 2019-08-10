79d48887c28d3995eeb83e274e26ee6556bf1141
Iuri Sousa <iuricontato21@gmail.com>
2019-08-10 16:00:45 -0300

    Aula apenas sobre padrão de nomeação]

---
+++ Aula03.java
@@ -0,0 +1,12 @@
+package curso;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		/*
+		 * Class only about pattern camelCase(Objects, variables, etc) and Pascalcase(name of class, methods, etc.)
+		 */
+
+	}
+
+}
