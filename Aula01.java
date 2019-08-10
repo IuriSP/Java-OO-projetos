0b865ab23396d4fc9d45c5fdc72c09ae037e3bd3
Iuri Sousa <iuricontato21@gmail.com>
2019-08-10 15:28:56 -0300

    Primeira aula

---
+++ Aula01.java
@@ -0,0 +1,16 @@
+package curso;
+
+import java.util.Scanner;
+
+public class Aula01 {
+
+	public static void main(String[] args) {
+		//"First" program in java
+		
+		Scanner sc = new Scanner(System.in); //Create object "SC" like Scanner for enter data
+		System.out.println("Hello World");
+		
+		sc.close();//Close object Scanner
+	}
+
+}
