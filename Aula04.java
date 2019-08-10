9d1786e01d5df5f5b65479896a3ca6e0815a7259
Iuri Sousa <iuricontato21@gmail.com>
2019-08-10 20:08:43 -0300

    Conversão implícita e casting

---
+++ Aula04.java
@@ -0,0 +1,21 @@
+package curso;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		
+		double a;
+		float b;
+		
+		a = 5.0;
+		b = (float)a; 
+		
+		//é preciso fazer um casting para o b aceitar o valor de a
+		
+		System.out.println(b);
+		
+		
+
+	}
+
+}
