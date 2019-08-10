f174e2d4245d450eca9403a3b566bc79203a4a63
Iuri Sousa <iuricontato21@gmail.com>
2019-08-10 20:31:27 -0300

    Saída de dados em Java

---
+++ Aula05.java
@@ -0,0 +1,23 @@
+package curso;
+
+public class Aula05 {
+
+	public static void main(String[] args) {
+
+		char gender = 'F';
+		int age = 32;
+		double balance = 10.35784;
+		String name = "Maria";
+		
+		System.out.print("Good Morning!");
+		System.out.println("Good afternoon!");
+		System.out.println("Good night!");
+		
+		System.out.println("-------------------");
+		
+		System.out.printf("%.2f%n", balance);
+		System.out.printf("%.4f %n", balance);
+		
+	}
+
+}
