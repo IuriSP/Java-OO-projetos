fff09c2195b1f8dde4008fe200b5898a981d80be
Iuri Sousa <iuricontato21@gmail.com>
2019-08-11 19:33:19 -0300

    Operadores Lógicos

---
+++ Aula11.java
@@ -0,0 +1,19 @@
+package curso;
+
+public class Aula11 {
+
+	public static void main(String[] args) {
+		/*
+		 * Apenas operadores lógicos
+		 * 
+		 * &&	E
+		 * ||	OU
+		 * !	NÃO
+		 * 
+		 * Precedência:
+		 * ! > && > ||
+		 */
+
+	}
+
+}
