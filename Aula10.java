3c20664c6f7641c9b151f8fb38ecfe4b30f46348
Iuri Sousa <iuricontato21@gmail.com>
2019-08-11 19:19:23 -0300

    Operadores comparativos (Boolean)

---
+++ Aula10.java
@@ -0,0 +1,16 @@
+package curso;
+
+public class Aula10 {
+
+	public static void main(String[] args) {
+			
+		int a = 10;
+		boolean c1 = a < 10;
+		boolean c2 = a < 20;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+
+	}
+
+}
